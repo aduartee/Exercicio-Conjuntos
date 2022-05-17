3237ae88f4940e2228e04e058f90085d2b4c8a2d
aduartee <arthurduartealves99@gmail.com>
2022-05-16 23:48:26 -0300

    Criado o programa que le quantos alunos tem em cada um curso e depois mostrar a quantidade de alunos do curso. Projeto feito usando conjuntos

--- Conjutos/Program.cs
+++ Conjutos/Program.cs
@@ -2,11 +2,50 @@
 
 namespace Program
 {
-   class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<int> A = new HashSet<int>();
+            HashSet<int> B = new HashSet<int>();
+            HashSet<int> C = new HashSet<int>();
+
+            Console.WriteLine("How many students for course A ?");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int read = int.Parse(Console.ReadLine());
+                A.Add(read);
+            }
+
+            Console.WriteLine("How many students for course B ?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int read = int.Parse(Console.ReadLine());
+                B.Add(read);
+            }
+
+            Console.WriteLine("How many students for course C ?");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int read = int.Parse(Console.ReadLine());
+                C.Add(read);
+            }
+
+            HashSet<int> all = new HashSet<int>(A);
+
+            all.Union(B);
+            all.Union(C);
+            Console.WriteLine("Total: " + all.Count);
+
+
+
+
         }
     }
 }
